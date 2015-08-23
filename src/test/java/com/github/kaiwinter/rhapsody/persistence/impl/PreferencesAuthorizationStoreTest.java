69cb9194c845e9233ce6dbba7c0d7fd6470e93a4
Kai Winter <kaiwinter@gmx.de>
2015-08-23 20:02:51 +0200

    unit test

---
+++ src/test/java/com/github/kaiwinter/rhapsody/persistence/impl/PreferencesAuthorizationStoreTest.java
@@ -0,0 +1,47 @@
+package com.github.kaiwinter.rhapsody.persistence.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.github.kaiwinter.rhapsody.persistence.AuthorizationStore;
+import com.github.kaiwinter.rhapsody.persistence.model.AuthorizationInfo;
+
+/**
+ * Tests for {@link PreferencesAuthorizationStore}.
+ */
+public final class PreferencesAuthorizationStoreTest {
+
+	/**
+	 * Tests if the values are stored and cleared correctly.
+	 */
+	@Test
+	public void test() {
+		AuthorizationStore authorizationStore = new PreferencesAuthorizationStore();
+		authorizationStore.clearAuthorization();
+
+		// Assert not set on start
+		AuthorizationInfo authorizationInfo = authorizationStore.loadAuthorizationInfo();
+		Assert.assertNull(authorizationInfo.accessToken);
+		Assert.assertNull(authorizationInfo.refreshToken);
+		Assert.assertNull(authorizationInfo.catalog);
+
+		// set values and save
+		authorizationInfo.accessToken = "accessToken";
+		authorizationInfo.refreshToken = "refreshToken";
+		authorizationInfo.catalog = "catalog";
+		authorizationStore.saveAuthorizationInfo(authorizationInfo);
+
+		// Reload, assert saved values are persisted
+		authorizationInfo = authorizationStore.loadAuthorizationInfo();
+		Assert.assertEquals("accessToken", authorizationInfo.accessToken);
+		Assert.assertEquals("refreshToken", authorizationInfo.refreshToken);
+		Assert.assertEquals("catalog", authorizationInfo.catalog);
+
+		// Clear values, reload and assert they are not set
+		authorizationStore.clearAuthorization();
+		authorizationInfo = authorizationStore.loadAuthorizationInfo();
+		Assert.assertNull(authorizationInfo.accessToken);
+		Assert.assertNull(authorizationInfo.refreshToken);
+		Assert.assertNull(authorizationInfo.catalog);
+	}
+}
